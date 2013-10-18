594dc3b2ac96d8ec504330645bcba89e8c45863c
Lorin Hochstein <lorin@nimbisservices.com>
2013-10-18 14:21:56 -0400

    Swift support for path and url

--- ostack/containers.py
+++ ostack/containers.py
@@ -14,13 +14,23 @@ class Container(object):
 
     def list(self):
         """ List the objects in a container """
-        return [Object(**x) for x in swift().get_container(self.name)[1]]
+        return [Object(container=self.name, **x) for x in
+                swift().get_container(self.name)[1]]
+
 
 class Object(object):
-    """ A swift object """
-    def __init__(self, name, bytes=None, last_modified=None, hash=None,
-                 content_type=None):
-        self.name = name
+    """ A swift object.
+
+    Can be initialized by specifying a path (including container),
+    or workflow/container separately
+    """
+    def __init__(self, path=None, container_name=None, name=None, bytes=None,
+                 last_modified=None, hash=None, content_type=None):
+        if path is not None:
+            (self.container_name, self.name) = path.split('/', 1)
+        else:
+            self.container_name = container_name
+            self.name = name
         self.bytes = bytes
         self.last_modified = last_modified
         self.hash = hash
@@ -28,6 +38,22 @@ class Object(object):
 
     def __repr__(self):
         return self.name
+
+    @property
+    def path(self):
+        return "{}/{}".format(self.container_name, self.name)
+
+    @property
+    def url(self):
+        """ Return the full url for this object """
+        storage_url, _ = swift().get_auth()
+        s = "{storage_url}/{path}"
+        return s.format(storage_url=storage_url,
+                        path=self.path)
+
+    def temp_url(self):
+        """ Generate a temporary url """
+        pass
 
 
 def list():
